e66d10d19a32add227be1a83c671a35e8b27bab8
if <55980773+Yifeeeeei@users.noreply.github.com>
2024-05-08 19:04:30 -0700

    a brand new version

--- README.md
+++ README.md
@@ -1,31 +1,18 @@
 # butterknife 🧈🗡️
-A printing tool for Golang debugging. It prints out a variable and spread out its callers.
+A printing tool for Golang debugging. It works like "fmt" or "log", but it prints out something else alongside, including:
 
-# functions
+- the line number and file that calls it
+- the function that wraps over it
+- the function that wraps over the function that wraps over it
+- ...
 
-```go
-func Print(varValue interface{}, varName ...string) 
-```
+Spreads the whole thing onto the terminal smoothly.
 
-Uses fmt to print formats and prints the value of the variable and its caller.
-
-```go
-func PrintConcurrent(varValue interface{}, varName ...string)
-```
-
-Uses fmt to print formats and prints the value of the variable and all its callers.
-
-```go
-func Log(varValue interface{}, varName ...string)
-```
-
-Uses log to print formats and prints the value of the variable and all its callers.
-
-```go
-func LogConcurrent(varValue interface{}, varName ...string)
-```
+Note: cmd + click on the line number to jump right to it
 
 # usage
+
+After initiating the project
 
 Fetch the module
 
@@ -42,20 +29,72 @@ import "github.com/Yifeeeeei/butterknife"
 Use the functions
 
 ```go
-func caller(num int) {
-	butterknife.PrintConcurrent(num, "num")
-}
+package main
+
+import (
+	"github.com/Yifeeeeei/butterknife"
+)
 
 func main() {
-	a := 42
-	butterknife.Print(a, "a")
-	// [.../main.go:11 main.main] a = 42
+	a := "a"
+	b := "b"
+	butterknife.Print(a, b)
+	// [main.go:10 main.main]  ab
 
-	caller(a)
-	// 	[.../main.go:14 main.main]
-	//    [.../main.go:6 main.caller]
-	//      num = 42
+	butterknife.Println(a, b)
+	// [main.go:13 main.main]  a b
+
+	butterknife.Printf("a:%s ,b:%s\n", a, b)
+	// [[main.go:16 main.main]  a:a ,b:b
+
+	caller()
 }
 
+func caller() {
+	c := "c"
+	d := "d"
+	butterknife.PrintHierarchy(c, d)
+	// [main.go:19 main.main]
+	//   [main.go:25 main.caller]
+	//     cd
+
+	butterknife.PrintlnHierarchy(c, d)
+	// [main.go:19 main.main]
+	//   [main.go:30 main.caller]
+	//     c d
+
+	butterknife.PrintfHierarchy("c:%s ,d:%s\n", c, d)
+	// [main.go:19 main.main]
+	//	[main.go:35 main.caller]
+	//	  c:c ,d:d
+}
+
+```
+
+# functions
+
+"Print" / "Log": use fmt/log
+
++
+
+"" / "f" / "ln": normal output / formatted output / line output
+
++
+
+"" / "Hierarchy": just print one caller / and all callers' callers
+
+```go
+func Print(args ...interface{})
+func Printf(format string, args ...interface{}) 
+func Println(args ...interface{})
+func PrintHierarchy(args ...interface{})
+func PrintfHierarchy(format string, args ...interface{})
+func PrintlnHierarchy(args ...interface{})
+func Log(args ...interface{})
+func Logf(format string, args ...interface{})
+func Logln(args ...interface{}) 
+func LogHierarchy(args ...interface{})
+func LogfHierarchy(format string, args ...interface{})
+func LoglnHierarchy(args ...interface{})
 ```
 
--- butterknife.go
+++ butterknife.go
@@ -3,26 +3,43 @@ package butterknife
 import (
 	"fmt"
 	"log"
+	"path/filepath"
 	"runtime"
 )
 
 type writer interface {
-	Writef(format string, args ...interface{})
+	writef(format string, args ...interface{})
+	write(args ...interface{})
+	writeln(args ...interface{})
 }
 
 type logger struct{}
 
-func (l logger) Writef(format string, args ...interface{}) {
-	log.Printf(format, args...)
+func (l logger) writef(format string, args ...interface{}) {
+	log.Printf("\n"+format, args...)
+}
+func (l logger) write(args ...interface{}) {
+	msg := fmt.Sprint(args...)
+	log.Printf("\n%s", msg)
+}
+func (l logger) writeln(args ...interface{}) {
+	msg := fmt.Sprintln(args...)
+	log.Printf("\n%s", msg)
 }
 
 type printer struct{}
 
-func (p printer) Writef(format string, args ...interface{}) {
+func (p printer) writef(format string, args ...interface{}) {
 	fmt.Printf(format, args...)
 }
+func (p printer) write(args ...interface{}) {
+	fmt.Print(args...)
+}
+func (p printer) writeln(args ...interface{}) {
+	fmt.Println(args...)
+}
 
-func write(varValue interface{}, wr writer, comments ...string) {
+func writef(wr writer, format string, args ...interface{}) {
 	_, file, line, ok := runtime.Caller(2)
 	if !ok {
 		fmt.Println("Failed to get caller info")
@@ -30,18 +47,41 @@ func write(varValue interface{}, wr writer, comments ...string) {
 	}
 	pc, _, _, _ := runtime.Caller(2)
 	funcName := runtime.FuncForPC(pc).Name()
-	if len(comments) > 0 {
-		vn := ""
-		for _, v := range comments {
-			vn += v + " "
-		}
-		wr.Writef("[%s:%d %s] %s: %v\n", file, line, funcName, vn, varValue)
-	} else {
-		wr.Writef("[%s:%d %s] %v\n", file, line, funcName, varValue)
-	}
+	file = filepath.Base(file)
+	header := fmt.Sprintf("[%s:%d %s]  ", file, line, funcName)
+	msg := fmt.Sprintf(format, args...)
+	wr.write(header + msg + "\n")
 }
 
-func writeConcurrent(varValue interface{}, wr writer, comments ...string) {
+func write(wr writer, args ...interface{}) {
+	_, file, line, ok := runtime.Caller(2)
+	if !ok {
+		fmt.Println("Failed to get caller info")
+		return
+	}
+	pc, _, _, _ := runtime.Caller(2)
+	funcName := runtime.FuncForPC(pc).Name()
+	file = filepath.Base(file)
+	header := fmt.Sprintf("[%s:%d %s]  ", file, line, funcName)
+	msg := fmt.Sprint(args...)
+	wr.write(header + msg + "\n")
+}
+
+func writeln(wr writer, args ...interface{}) {
+	_, file, line, ok := runtime.Caller(2)
+	if !ok {
+		fmt.Println("Failed to get caller info")
+		return
+	}
+	pc, _, _, _ := runtime.Caller(2)
+	funcName := runtime.FuncForPC(pc).Name()
+	file = filepath.Base(file)
+	header := fmt.Sprintf("[%s:%d %s]  ", file, line, funcName)
+	msg := fmt.Sprintln(args...)
+	wr.write(header + msg)
+}
+
+func writelnHierarchy(wr writer, args ...interface{}) {
 	layers := []string{}
 	for i := 0; ; i++ {
 		_, file, line, ok := runtime.Caller(i)
@@ -50,60 +90,162 @@ func writeConcurrent(varValue interface{}, wr writer, comments ...string) {
 		}
 		pc, _, _, _ := runtime.Caller(i)
 		funcName := runtime.FuncForPC(pc).Name()
+		file = filepath.Base(file)
 		layers = append(layers, fmt.Sprintf("[%s:%d %s]", file, line, funcName))
 	}
+	msg := ""
 	spaces := ""
 	for i := len(layers) - 3; i >= 2; i-- {
-		wr.Writef("%s%s\n", spaces, layers[i])
+		msg += fmt.Sprintf("%s%s\n", spaces, layers[i])
 		spaces += "  "
 	}
-	if len(comments) > 0 {
-		vn := ""
-		for _, v := range comments {
-			vn += v + " "
+	msg += spaces
+	msg += fmt.Sprintln(args...)
+	wr.write(msg)
+}
+
+func writeHierarchy(wr writer, args ...interface{}) {
+	layers := []string{}
+	for i := 0; ; i++ {
+		_, file, line, ok := runtime.Caller(i)
+		if !ok {
+			break
 		}
-		wr.Writef("%s%s: %v\n", spaces, vn, varValue)
-	} else {
-		wr.Writef("%s %v\n", spaces, varValue)
+		pc, _, _, _ := runtime.Caller(i)
+		funcName := runtime.FuncForPC(pc).Name()
+		file = filepath.Base(file)
+		layers = append(layers, fmt.Sprintf("[%s:%d %s]", file, line, funcName))
 	}
+	msg := ""
+	spaces := ""
+	for i := len(layers) - 3; i >= 2; i-- {
+		msg += fmt.Sprintf("%s%s\n", spaces, layers[i])
+		spaces += "  "
+	}
+	msg += spaces
+	msg += fmt.Sprint(args...)
+	wr.write(msg + "\n")
 }
 
-// Uses fmt to print formats and prints the value of the variable and its caller.
-// The function takes two parameters:
-//   - varValue: the value of the variable.
-//   - comments: you might want to put the name of the variable here (optional)
-//
-// It does not return any value.
-func Print(varValue interface{}, comments ...string) {
-	write(varValue, printer{}, comments...)
+func writefHierarchy(wr writer, format string, args ...interface{}) {
+	layers := []string{}
+	for i := 0; ; i++ {
+		_, file, line, ok := runtime.Caller(i)
+		if !ok {
+			break
+		}
+		pc, _, _, _ := runtime.Caller(i)
+		funcName := runtime.FuncForPC(pc).Name()
+		file = filepath.Base(file)
+		layers = append(layers, fmt.Sprintf("[%s:%d %s]", file, line, funcName))
+	}
+	msg := ""
+	spaces := ""
+	for i := len(layers) - 3; i >= 2; i-- {
+		msg += fmt.Sprintf("%s%s\n", spaces, layers[i])
+		spaces += "  "
+	}
+	msg += spaces
+	msg += fmt.Sprintf(format, args...)
+	wr.write(msg + "\n")
 }
 
-// Uses fmt to print formats and prints the value of the variable and all its callers.
-// The function takes two parameters:
-//   - varValue: the value of the variable.
-//   - comments: you might want to put the name of the variable here (optional)
+// Use it like fmt.Print. It will print the args and the caller info.
+//   - args: the values to print.
 //
 // It does not return any value.
-func PrintConcurrent(varValue interface{}, comments ...string) {
-	writeConcurrent(varValue, printer{}, comments...)
+func Print(args ...interface{}) {
+	write(printer{}, args...)
 }
 
-// Uses log to print formats and prints the value of the variable and its caller.
-// The function takes two parameters:
-//   - varValue: the value of the variable.
-//   - comments: you might want to put the name of the variable here (optional)
+// Use it like fmt.Printf. It will format and print the args and the caller info.
+//   - format: the format string.
+//   - args: the values to print.
 //
 // It does not return any value.
-func Log(varValue interface{}, comments ...string) {
-	write(varValue, logger{}, comments...)
+func Printf(format string, args ...interface{}) {
+	writef(printer{}, format, args...)
 }
 
-// Uses log to print formats and prints the value of the variable and all its callers.
-// The function takes two parameters:
-//   - varValue: the value of the variable.
-//   - comments: you might want to put the name of the variable here (optional)
+// Use it like fmt.Println. It will print the args and the caller info.
+//   - args: the values to print.
 //
 // It does not return any value.
-func LogConcurrent(varValue interface{}, comments ...string) {
-	writeConcurrent(varValue, logger{}, comments...)
+func Println(args ...interface{}) {
+	writeln(printer{}, args...)
+}
+
+// Use it like fmt.Print. It will print the args and its caller, as well as its caller's caller, and so on.
+//   - args: the values to print.
+//
+// It does not return any value.
+func PrintHierarchy(args ...interface{}) {
+	writeHierarchy(printer{}, args...)
+}
+
+// Use it like fmt.Printf. It will format and print the args and its caller, as well as its caller's caller, and so on.
+//   - format: the format string.
+//   - args: the values to print.
+//
+// It does not return any value.
+func PrintfHierarchy(format string, args ...interface{}) {
+	writefHierarchy(printer{}, format, args...)
+}
+
+// Use it like fmt.Println. It will print the args and its caller, as well as its caller's caller, and so on.
+//   - args: the values to print.
+//
+// It does not return any value.
+func PrintlnHierarchy(args ...interface{}) {
+	writelnHierarchy(printer{}, args...)
+}
+
+// Use it like log.Print. It will log the args and the caller info.
+//   - args: the values to log.
+//
+// It does not return any value.
+func Log(args ...interface{}) {
+	write(logger{}, args...)
+}
+
+// Use it like log.Printf. It will format and log the args and the caller info.
+//   - format: the format string.
+//   - args: the values to log.
+//
+// It does not return any value.
+func Logf(format string, args ...interface{}) {
+	writef(logger{}, format, args...)
+}
+
+// Use it like log.Println. It will log the args and the caller info.
+//   - args: the values to log.
+//
+// It does not return any value.
+func Logln(args ...interface{}) {
+	writeln(logger{}, args...)
+}
+
+// Use it like log.Print. It will log the args and its caller, as well as its caller's caller, and so on.
+//   - args: the values to log.
+//
+// It does not return any value.
+func LogHierarchy(args ...interface{}) {
+	writeHierarchy(logger{}, args...)
+}
+
+// Use it like log.Printf. It will format and log the args and its caller, as well as its caller's caller, and so on.
+//   - format: the format string.
+//   - args: the values to log.
+//
+// It does not return any value.
+func LogfHierarchy(format string, args ...interface{}) {
+	writefHierarchy(logger{}, format, args...)
+}
+
+// Use it like log.Println. It will log the args and its caller, as well as its caller's caller, and so on.
+//   - args: the values to log.
+//
+// It does not return any value.
+func LoglnHierarchy(args ...interface{}) {
+	writelnHierarchy(logger{}, args...)
 }
